b1c23d8b1700ccc0814eb15a8a9f8199cbff4893
admin <admin>
2017-07-13 17:35:46 +0800

    first commit

---
+++ js/zhzp.js
@@ -0,0 +1,9 @@
+﻿function downfile()
+{
+	var ua = navigator.userAgent.toLowerCase();
+	if(ua.match(/MicroMessenger/i)=="micromessenger") {
+		alert("在页面右上方选择在浏览器中打开");	
+	} else {
+		location.href="resource/resource.zip"
+	}
+}
